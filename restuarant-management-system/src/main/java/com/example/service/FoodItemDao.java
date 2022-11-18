09fdc6ca6b835052ca6909bd61e6ebde86014e5f
pavans18 <118244628+pavans18@users.noreply.github.com>
2022-11-18 15:50:57 +0530

    Updated all functions

---
+++ restuarant-management-system/src/main/java/com/example/service/FoodItemDao.java
@@ -0,0 +1,30 @@
+package com.example.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.FoodItem;
+
+@Repository
+public interface FoodItemDao {
+	
+	public List<FoodItem> showAllItems();
+	
+	public void saveItem(FoodItem foodItem);
+	
+	public Optional<FoodItem> findItem(Integer itemNumber);
+	
+	public void deleteItemByID(Integer itemNumber);
+	
+	public void editItem(FoodItem foodItem);
+	
+	public FoodItem generateItemId(Integer itemNumber);
+	
+	public FoodItem getFoodItem(Integer itemNumber);
+
+	public Integer getNumber();
+
+
+}
---
+++ restuarant-management-system/src/main/java/com/example/service/FoodItemDaoImpl.java
@@ -0,0 +1,64 @@
+package com.example.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import com.example.entity.FoodItem;
+import com.example.repository.FoodItemRepository;
+
+@Repository
+@Service
+public class FoodItemDaoImpl implements FoodItemDao{
+
+	@Autowired
+	private FoodItemRepository foodrepo;
+
+	@Override
+	public List<FoodItem> showAllItems() {
+		return foodrepo.findAll();
+	}
+
+	@Override
+	public void saveItem(FoodItem foodItem) {
+
+		foodrepo.save(foodItem);
+	}
+
+	@Override
+	public Optional<FoodItem> findItem(Integer itemNumber) {
+
+		return foodrepo.findById(itemNumber);
+	}
+
+	@Override
+	public void deleteItemByID(Integer itemNumber) {
+		foodrepo.deleteById(itemNumber);	
+	}
+
+	@Override
+	public void editItem(FoodItem foodItem) {
+
+		foodrepo.save(foodItem);
+
+	}
+
+	@Override
+	public FoodItem generateItemId(Integer itemNumber) {
+
+		return null;
+	}
+
+	@Override
+	public FoodItem getFoodItem(Integer itemNumber) {
+		return foodrepo.findById(itemNumber).get();
+
+	}
+
+	@Override
+	public Integer getNumber() {
+		return foodrepo.findMaxItemNumber();
+	}
+
+}
